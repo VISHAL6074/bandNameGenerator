36d3b4f700270fbe2d296677239c8cb5454253df
VISHAL6074 <106880180+VISHAL6074@users.noreply.github.com>
2024-10-01 22:04:10 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,6 @@
+print("Click Run to run the final Project")
+print(" Welcome to the BandName Generator")
+bandName  =  input("What's The Name of the City you grew up in?")
+petName =  input("What's Your Pet's Name ?")
+
+print("Your Band Name could be " + bandName + petName )
